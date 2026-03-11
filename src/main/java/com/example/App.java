3d1b02eca2692ce218736312a8fb22eedf0d4365
Rahul-3043 <3055rahulgitte@gmail.com>
2026-03-11 14:37:52 +0530

    Implement basic HTTP server in App.java
    Set up a simple HTTP server that responds with a message.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,11 +1,22 @@
-package com.example;
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
 public class App {
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
-        System.out.println("My First Maven Application");
+        server.createContext("/", exchange -> {
+            String response = "My First Maven Application";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
 
+        server.start();
+
+        System.out.println("Server started on port 8080");
     }
-
 }
