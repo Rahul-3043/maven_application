62a8fcaf34fc4443e3ed1d35b5600e8afbe30c83
Rahul-3043 <3055rahulgitte@gmail.com>
2026-03-11 15:19:32 +0530

    Update output message with timestamp

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,7 +4,7 @@ public class App {
 
     public static void main(String[] args) throws InterruptedException {
 
-        System.out.println("My First Maven Application");
+        System.out.println("My First Maven Application time of change is 3:19");
 
         Thread.sleep(300000); // 5 minutes
 
