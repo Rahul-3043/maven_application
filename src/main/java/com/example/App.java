c6966d8992f0e0788a4b54207dc4df598849c5fc
Rahul-3043 <3055rahulgitte@gmail.com>
2026-03-11 15:03:30 +0530

    Refactor App to remove HTTP server and add loop
    Removed HTTP server implementation and added a loop to keep the application running.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,22 +1,11 @@
-import com.sun.net.httpserver.HttpServer;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-
 public class App {
     public static void main(String[] args) throws Exception {
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        System.out.println("My First Maven Application");
 
-        server.createContext("/", exchange -> {
-            String response = "My First Maven Application";
-            exchange.sendResponseHeaders(200, response.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        });
-
-        server.start();
-
-        System.out.println("Server started on port 8080");
+        while(true) {
+            System.out.println("Application running...");
+            Thread.sleep(10000);
+        }
     }
 }
