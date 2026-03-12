62c7379ba1e106357bd2ff94c4fe7dac440c940c
Rahul-3043 <3055rahulgitte@gmail.com>
2026-03-12 11:14:42 +0530

    Update application message with new time

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,7 +4,7 @@ public class App {
 
     public static void main(String[] args) throws InterruptedException {
 
-        System.out.println("My First Maven Application time of change is 11 am");
+        System.out.println("My First Maven Application time of change is 11:14 am");
 
         Thread.sleep(300000); // 5 minutes
 
